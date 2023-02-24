283551de19eaa3cffce00cc4a9f29118d348456c
Joisson Rocha <strattegiadev@gmail.com>
2023-02-24 17:46:46 -0300

    :art: Styles: updating carousel cards

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -9,12 +9,12 @@ const FeaturedPostCard = ({ post }) => (
       className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72"
       style={{ backgroundImage: `url('${post.featuredImage.url}')` }}
     />
-    <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
+    <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-70 from-gray-500 via-gray-800 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
-      <p className="text-white mb-4 text-shadow font-semibold text-xs">
+      <p className="text-white mb-2 text-shadow text-xs">
         {moment(post.createdAt).format("MMM DD, YYYY")}
       </p>
-      <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">
+      <p className="text-white mb-6 text-shadow font-semibold text-xl text-center">
         {post.title}
       </p>
       <div className="flex items-center absolute bottom-5 w-full justify-center">
@@ -23,10 +23,10 @@ const FeaturedPostCard = ({ post }) => (
           alt={post.author.name}
           height={30}
           width={30}
-          className="align-middle drop-shadow-lg rounded-full"
+          className="align-middle drop-shadow-xl rounded-full"
           src={post.author.photo.url}
         />
-        <p className="inline align-middle text-white text-shadow ml-2 font-medium">
+        <p className="inline align-middle text-white text-shadow ml-2 font-medium italic">
           {post.author.name}
         </p>
       </div>
